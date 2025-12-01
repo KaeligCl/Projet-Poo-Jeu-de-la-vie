f206743f0d58c0af9098a39219003cce1d68076b
Lisa <183915043+Kaalysa@users.noreply.github.com>
2025-12-01 16:34:15 +0100

    Ajout de regles.hpp avec implémentation

---
+++ regles.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+class regle
+{
+private :
+    long compteurIteration; //compteur d'itérations maximales
+public:
+    void etatSuivant(); //changer vraiment l'état de la cellule
+};
