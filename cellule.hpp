01c8f03fc66f5db2288bc838785889e6aa702bb9
Lisa <183915043+Kaalysa@users.noreply.github.com>
2025-12-02 11:38:02 +0100

    Correction classe cellule.hpp

--- cellule.hpp
+++ cellule.hpp
@@ -1,14 +1,11 @@
 #pragma once
 
-class cellule
+class Cellule
 {
 protected:
-    bool estVivant;
-    bool next; //état suivant stocké temporaire
+    const bool estVivant;
 public :
-    Cellule(bool estVivant, bool next) : estVivant(estVivant), next(next) {}
+    Cellule(const bool estVivant) : estVivant(estVivant) {}
     bool getEstVivant() const { return estVivant; }
-    bool getNext() const {  return next; }
-    void setestVivant(bool estVivant) { this -> estVivant = estVivant; }
-    void setNext(bool next) { this -> next = next; }
 };
+
