309233b537c0268db62c260cbe4ef29f0d9b86c2
Lisa <183915043+Kaalysa@users.noreply.github.com>
2025-12-02 11:30:23 +0100

    Correction classe cellule.hpp

--- cellule.hpp
+++ cellule.hpp
@@ -3,9 +3,12 @@
 class cellule
 {
 protected:
+    bool estVivant;
     bool next; //état suivant stocké temporaire
 public :
-    Cellule(bool next) : next(next) {}
+    Cellule(bool estVivant, bool next) : estVivant(estVivant), next(next) {}
+    bool getEstVivant() const { return estVivant; }
     bool getNext() const {  return next; }
+    void setestVivant(bool estVivant) { this -> estVivant = estVivant; }
     void setNext(bool next) { this -> next = next; }
 };
