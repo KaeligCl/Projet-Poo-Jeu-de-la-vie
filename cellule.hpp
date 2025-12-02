1d6def3800569033c877c7d55068f86a237a21c0
Lisa <183915043+Kaalysa@users.noreply.github.com>
2025-12-02 11:02:40 +0100

    Correction classe cellule.hpp
    Removed estVivant attribute and related methods from cellule class.

--- cellule.hpp
+++ cellule.hpp
@@ -3,13 +3,9 @@
 class cellule
 {
 protected:
-    bool estVivant;
-    bool next; //état suivant
+    bool next; //état suivant stocké temporaire
 public :
-    cellule(bool estVivant, bool next) : estVivant(estVivant), next(next) {}
-    bool getEstVivant() const { return estVivant; }
+    Cellule(bool next) : next(next) {}
     bool getNext() const {  return next; }
-    void setEstVivant(bool estVivant) { this -> estVivant = estVivant; }
     void setNext(bool next) { this -> next = next; }
-    virtual bool update() = 0;
 };
