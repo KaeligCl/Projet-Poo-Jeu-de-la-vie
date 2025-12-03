e1336fd03ff24734b049db22858f61756bd8ebac
Lisa <183915043+Kaalysa@users.noreply.github.com>
2025-12-03 15:17:33 +0100

    Rajout d'une partie du main
    Partie pour l'instant mise en commentaire (remettre hors commentaire plus tard)

--- main.cpp
+++ main.cpp
@@ -7,6 +7,45 @@
 #include <optional>
 
 int main() {
+    /*Fichier f;
+    regle r;
+    cout << "Bienvenue dans le jeu de la vie"
+    << "\nMettez le chemin d'acces de la matrice originelle (utilisez des /): " <<endl;
+    string chemin;
+    getline(cin, chemin);
+
+    cout << "Entrer un nombre maximum d'iterations : " << endl;
+    int nbrMaxItération;
+    cin >> nbrMaxItération;
+    while (nbrMaxItération <2) //si jamais l'utilisateur abuse
+    {
+        cout << "Veuillez entrer un nombre superieur à deux iterations : " <<endl;
+        cin >> nbrMaxItération;
+    }
+
+
+    cout << "Mode de jeu choisi : mode console avec "
+            "fichiers de sorties pour les premières iterations (1)" <<
+                "Avec interface graphique (2) : "  <<endl;
+    int choixMode;
+    cin >> choixMode;
+
+    if (choixMode == 1)
+    {
+        cout << "Vous avez choisi le mode console." << endl;
+        f.lireFichier(chemin);
+    }
+
+    else if (choixMode == 2)
+    {
+        cout << "Vous avez choisi le mode interface graphique" << endl;
+        f.lireFichier(chemin);
+    }*/
+
+
+
+
+    
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Grid Window");
 
     Fichier f;
