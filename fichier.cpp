3a9773d23f9789f472ff15c2e020b9dc698f7ca6
Lisa <183915043+Kaalysa@users.noreply.github.com>
2025-12-03 13:54:41 +0100

    1ère implémentation de fichier.cpp
    Des modifications auront lieu notamment pour le chemin d'accès qui sera finalement demandé dans le main.cpp à l'utilisateur

--- fichier.cpp
+++ fichier.cpp
@@ -1,2 +1,42 @@
+#include "fichier.hpp"
+#include <sstream>
+#include<string>
+#include <fstream>
+#include <iostream>
+using namespace std;
 
-ijabdfvjeabnf fkdfvnaeevneavn
+Fichier::Fichier() : l(0), L(0) {}
+
+void Fichier::lireFichier()
+{
+    //récupérer la largeur et longueur de la matrice
+     ifstream fichier("C:/Users/lisac/OneDrive/Documents/CPIA2/Bloc 2 - Programmation Objet/Projet/fichierTest.txt");
+     if (fichier)
+     {
+         //pour vérifier que le fichier est ouvert
+         string ligne;
+         int largeur;
+         int longueur;
+         if (getline(fichier, ligne)) {
+
+             stringstream ss(ligne);
+             ss >> largeur >> longueur;
+
+             //mettre 1er caractère sur la largeur et le second sur la longueur
+             this -> setL(largeur);
+             this -> setl(longueur);
+
+             cout << "Largeur de la matrice : " << this ->getL() << endl;
+             cout << "Longueur de la matrice : " << this-> getl() << endl;
+         }
+     }
+
+     else
+     {
+         cout << "ERREUR : le fichier ne s'est pas ouvert" <<endl;
+     }
+
+ }
+
+ void Fichier::creerFichier() {
+ }
