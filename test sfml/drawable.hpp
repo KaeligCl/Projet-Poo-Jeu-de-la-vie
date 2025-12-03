8f287549e3f81489be2982e24ba44333cc0c2a71
Kaëlig <162746512+KaeligCl@users.noreply.github.com>
2025-12-03 10:38:11 +0100

    Refactor Grid class constructor and member variables

--- test sfml/drawable.hpp
+++ test sfml/drawable.hpp
@@ -3,8 +3,11 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <optional>
-#include "cellule.hpp"
 #include <vector>
+#include <memory>
+
+#include "../cellule.hpp"
+
 using namespace sf;
 
 struct Pos {
@@ -13,17 +16,19 @@ struct Pos {
 };
 
 template <size_t Rows, size_t Cols>
-
 class Grid {
-    private:
-        vector<vector<shared_ptr<Cellule>>> data;;
-        
-        float cellSize;
-        float lineSize;
-        Pos origin;
-    
-    public:
-        Grid(const vector<vector<shared_ptr<Cellule>>>)
-        : data(table), cellSize(20.f), lineSize(2.f), origin(Pos{50.f, 50.f}) {}
-        void draw(RenderWindow& window) const;
+private:
+    std::vector<std::vector<std::shared_ptr<Cellule>>> data;
+    float cellSize;
+    float lineSize;
+    Pos origin;
+
+public:
+    Grid(const std::vector<std::vector<std::shared_ptr<Cellule>>>& table,
+         float cellSz = 20.f,
+         float lineSz = 2.f,
+         Pos org = {50.f, 50.f})
+        : data(table), cellSize(cellSz), lineSize(lineSz), origin(org) {}
+
+    void draw(RenderWindow& window) const;
 };
